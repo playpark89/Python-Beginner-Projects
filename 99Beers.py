ca3a35ddbbe17487a18b4cff1718c5746db47a28
playpark89 <41502062+playpark89@users.noreply.github.com>
2019-10-07 17:14:18 +0800

    Update and rename Beer.py to 99Beers.py

--- 99Beers.py
+++ 99Beers.py
@@ -20,21 +20,3 @@ for x in bottles_of_beer:
         print("Take one down and pass it around, no more bottles of beer on the wall\n")
     else:
         print("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall")
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-lyrics1 = str(bottles_of_beer[0]) + " bottles of beer on the wall, " + str(bottles_of_beer) + " bottles of beer.\n"
-
-lyrics2 = "Take one down and pass it around, " + str(bottles_of_beer) + " bottles of beer on the wall."
-print(lyrics1 + lyrics2)
-"""
